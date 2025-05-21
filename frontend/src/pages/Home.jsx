db05126d1da0ffb4138643ec64703a2d48f32e12
Leoarf <leonardoaraujofonsecaofc@gmail.com>
2025-05-21 16:01:34 -0300

    feat: add NewsArrivals component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
+import NewsArrivals from '../components/Products/NewsArrivals';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <GenderCollectionSection />
+      <NewsArrivals />
     </div>
   );
 };
