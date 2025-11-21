3692f28a26ec8bf02bd1876883b230084d28e68b
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2025-11-21 12:47:59 -0300

    feat: add Order router

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -33,7 +33,7 @@ const orderSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
+      ref: 'User',
       required: true,
     },
     orderItems: [orderItemSchema],
