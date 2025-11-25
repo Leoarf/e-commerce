86c3ce7ee1d5279858acc24d377706cbe8116982
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2025-11-24 21:19:38 -0300

    refactor: refactor the adminSidebar logout button to now log the user out by contacting the backend

--- frontend/src/components/Admin/AdminSidebar.jsx
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -7,11 +7,17 @@ import {
   FaUser,
 } from 'react-icons/fa';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../redux/slices/authSlice';
+import { clearCart } from '../../redux/slices/cartSlice';
 
 const AdminSidebar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogout = () => {
+    dispatch(logout());
+    dispatch(clearCart());
     navigate('/');
   };
 
